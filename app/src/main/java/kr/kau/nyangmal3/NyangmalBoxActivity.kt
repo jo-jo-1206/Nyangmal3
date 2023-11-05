8899ac32307d190a3c33893e05f4e3c019a3bd91
whthdbs03 <whvod222@naver.com>
2023-11-06 00:36:27 +0900

    Update NyangmalBoxActivity.kt

--- app/src/main/java/kr/kau/nyangmal3/NyangmalBoxActivity.kt
+++ app/src/main/java/kr/kau/nyangmal3/NyangmalBoxActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 //냥말박스: 익명편지함 냥말오면 쌓이고? 읽고 삭제 가능, 냥말보내기는 친구리스트에서 구현?(팝업), 50자 정도.
+//메모장 비슷하게??? db는 나중에 한다치고, 채팅이랑 비슷한가???
 class NyangmalBoxActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
