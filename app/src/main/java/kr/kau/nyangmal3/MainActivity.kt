2341b367deac03351e2d6829be0af0a4a0f4af28
jo_jo_1206 <jojo1206@protonmail.com>
2023-11-05 10:28:15 +0900

    Added buttons for home & chat

--- app/src/main/java/kr/kau/nyangmal3/MainActivity.kt
+++ app/src/main/java/kr/kau/nyangmal3/MainActivity.kt
@@ -1,11 +1,28 @@
 package kr.kau.nyangmal3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.kau.nyangmal3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 홈버튼 클릭 시 홈으로 이동
+        binding.btnHome.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 챗버튼 클릭 시 챗으로 이동
+        binding.btnChat.setOnClickListener {
+            val intent = Intent(this, ChatActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
