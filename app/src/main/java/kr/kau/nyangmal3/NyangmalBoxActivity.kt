69cd7e2b17a997a449e2e61be02bbe1816bedb29
whthdbs03 <whvod222@naver.com>
2023-11-06 04:44:29 +0900

    냥말박스 뒤로가기버튼

--- app/src/main/java/kr/kau/nyangmal3/NyangmalBoxActivity.kt
+++ app/src/main/java/kr/kau/nyangmal3/NyangmalBoxActivity.kt
@@ -1,13 +1,23 @@
 package kr.kau.nyangmal3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.kau.nyangmal3.databinding.ActivityNyangmalBoxBinding
 
 //냥말박스: 익명편지함 냥말오면 쌓이고? 읽고 삭제 가능, 냥말보내기는 친구리스트에서 구현?(팝업), 50자 정도.
 //메모장 비슷하게??? db는 나중에 한다치고, 채팅이랑 비슷한가???
 class NyangmalBoxActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNyangmalBoxBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nyangmal_box)
+        binding = ActivityNyangmalBoxBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //backib눌렀을때 다시 홈액티비티로 전환됨.
+        binding.backIb.setOnClickListener {
+            val nextintent = Intent(this, HomeActivity::class.java)
+            startActivity(nextintent)
+        }
     }
 }
