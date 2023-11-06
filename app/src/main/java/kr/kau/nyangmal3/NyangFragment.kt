139ca569743b3df63017c87d938573aa2733fe05
whthdbs03 <whvod222@naver.com>
2023-11-06 22:38:55 +0900

    눈송이

--- app/src/main/java/kr/kau/nyangmal3/NyangFragment.kt
+++ app/src/main/java/kr/kau/nyangmal3/NyangFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kr.kau.nyangmal3.databinding.FragmentNyangBinding
 
 //냥: 펑/스토리 ~24시간뒤면 사라짐. 한번읽으면사라짐?, 모든 사람들의 상태메시지모음화면느낌
 class NyangFragment : Fragment() {
+
+    private lateinit var binding: FragmentNyangBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
