14503098122bb0a1a784487a8de1139af2090218
whthdbs03 <whvod222@naver.com>
2023-11-14 10:04:24 +0900

    Update SnowItem.kt

--- app/src/main/java/kr/kau/nyangmal3/SnowItem.kt
+++ app/src/main/java/kr/kau/nyangmal3/SnowItem.kt
@@ -6,11 +6,6 @@ data class SnowItem(
     val timestamp: Long,
 //    val postId: String,
 //    val userId: String, 이건여기서 가져오는게 맞나?
-//    val userName: String, ㅇㅇㅇㅇ
     val imageUrl: String
-//    val text: String, ㅇㅇㅇ
-//    val timestamp: Long ㅇㅇㅇ
-){ //이건 ㅙ쓰는거지
-    //constructor():this("")
-}
+)
 
