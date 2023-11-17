5cbdb316260d9af6ad04fa56fe7bbe20034d7ade
whthdbs03 <whvod222@naver.com>
2023-11-17 17:22:18 +0900

    Update SnowItem.kt

--- app/src/main/java/kr/kau/nyangmal3/SnowItem.kt
+++ app/src/main/java/kr/kau/nyangmal3/SnowItem.kt
@@ -8,17 +8,17 @@ data class SnowItem(
 //    val userId: String, 이건여기서 가져오는게 맞나?
     val imageUrl: String
 ){
-    //constructor():this("","",0,"")
+    constructor():this("","",0,"")
     // Map 형태로 변환하는 함수
     //Firebase Realtime Database에서는 데이터를 효율적으로 저장하기 위해 Map 형태로 변환하여 저장하는 것이 권장됩니다.
 
-    fun toMap(): Map<String, Any?> {
-        return mapOf(
-            "userName" to userName,
-            "postText" to postText,
-            "timestamp" to timestamp,
-            "imageUrl" to imageUrl
-        )
-    }
+//    fun toMap(): Map<String, Any?> {
+//        return mapOf(
+//            "userName" to userName,
+//            "postText" to postText,
+//            "timestamp" to timestamp,
+//            "imageUrl" to imageUrl
+//        )
+//    }
 
 }
