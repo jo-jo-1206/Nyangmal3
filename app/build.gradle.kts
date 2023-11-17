9468022c416affdde0ecc72ba653f1a551144c5e
YoonJiWon1102 <ueong1016@naver.com>
2023-11-17 19:06:26 +0900

    받는화면 밑작업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,19 +55,15 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-
     implementation ("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.compose.material3:material3:1.1.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-database:20.3.0")
-
     implementation("com.google.firebase:firebase-analytics-ktx")
-
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
 }
--- app/src/main/java/kr/kau/nyangmal3/CMessageAdapter.kt
+++ app/src/main/java/kr/kau/nyangmal3/CMessageAdapter.kt
@@ -1,6 +1,7 @@
 package kr.kau.nyangmal3
 
 import android.content.Context
+import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 import kr.kau.nyangmal3.databinding.ReceiveBinding
 import kr.kau.nyangmal3.databinding.SendBinding
 import org.w3c.dom.Text
@@ -15,37 +17,57 @@ import org.w3c.dom.Text
 class CMessageAdapter(private val context: android.content.Context):
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    private var messageList = mutableListOf<CMessageData>()
+    private val send = 1
+    private val receive = 2
+
+    private var messageList = mutableListOf<CMessageData>() // 리스트에는 Message타입의 데이터가 들어감
     fun setListData(data: MutableList<CMessageData>){
         messageList = data
     }
 
     // recylerview가 viewholder를 새로만들어야 할 때마다 메소드 호출
     // 뷰홀더와 뷰를 생성하고 초기화
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SendViewHolder {
-        val binding = SendBinding.inflate(LayoutInflater.from(context),parent,false)
-        return SendViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if(viewType == send){// 받는 화면
+            val binding = SendBinding.inflate(LayoutInflater.from(context),parent,false)
+            return SendViewHolder(binding)
+        }else{
+            val binding = ReceiveBinding.inflate(LayoutInflater.from(context),parent,false)
+            return ReceiveViewHolder(binding)
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val currentMessage = messageList[position]
-        //val currentTime = currentMessage.send_time
-        if(holder.javaClass == SendViewHolder::class.java){
+
+        if(holder.javaClass == SendViewHolder::class.java){ // 보내는 데이터
             val viewHolder = holder as SendViewHolder
-            viewHolder.bind(currentMessage)
-            //(viewHolder.getDateText(currentTime)
+            viewHolder.sendBind(currentMessage)
         }
-        else{
+        else{ // 받는 데이터
             val viewHolder = holder as ReceiveViewHolder
-            viewHolder.bind2(currentMessage)
+            viewHolder.receiveBind(currentMessage)
         }
     }
 
     // 아이템의 갯수 반환 ex) 주소록의 총 주소 개수
     override fun getItemCount():Int = messageList.size
 
+    // 어떤 뷰홀더를 사용할 지 결정 함 -> 현재 접속자 아이디와 sendId가 같으면 sendViewHolder 사용
+    override fun getItemViewType(position: Int): Int {
+        val currentMessage = messageList[position]
+        return if(FirebaseAuth.getInstance().currentUser?.uid.equals(currentMessage.sendId)){
+            send
+            Log.d("sendId","아이디가 서로 같음")
+        }else{
+            receive
+            Log.d("receiveId","아이디가 같지 않음")
+        }
+    }
+
+    // 메세지를 보내는 쪽의 뷰홀더
     class SendViewHolder(private val binding: SendBinding) : RecyclerView.ViewHolder(binding.root){
-        fun bind(messageList: CMessageData?){
+        fun sendBind(messageList: CMessageData?){
             messageList?.let {
                 binding.sendMessage.text = it.message
                 val sendData = it.send_time
@@ -54,6 +76,7 @@ class CMessageAdapter(private val context: android.content.Context):
             }
         }
 
+        // 시간 포맷 예뿌게
         fun getDateText(sendData:String): String {
             var dateText =""
             var timeString = ""
@@ -76,12 +99,35 @@ class CMessageAdapter(private val context: android.content.Context):
             }
     }
 
+    // 메세지 받는 쪽의 뷰홀더
     class ReceiveViewHolder(private val binding: ReceiveBinding) : RecyclerView.ViewHolder(binding.root){
-        fun bind2(messageList: CMessageData?) {
+        fun receiveBind(messageList: CMessageData?) {
             messageList?.let {
                 binding.receiveMessage.text = it.message
-
+                val sendData = it.send_time
+                val dateText = getDateText(sendData)
+                binding.txtDate.text = dateText
             }
+        }
+        fun getDateText(sendData:String): String {
+            var dateText =""
+            var timeString = ""
+            if(sendData.isNotBlank()){
+                timeString = sendData.substring(8,12)
+                var hour = timeString.substring(0, 2)
+                var minute = timeString.substring(2, 4)
+
+                var timeformat = "%02d:%02d"
+
+                if (hour.toInt() > 11) {
+                    dateText += "오후 "
+                    dateText += timeformat.format(hour.toInt() - 12, minute.toInt())
+                } else {
+                    dateText += "오전 "
+                    dateText += timeformat.format(hour.toInt(), minute.toInt())
+                }
+            }
+            return dateText
         }
     }
     }
